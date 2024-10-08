c65f6a21f6ac1e76218c770cca6010659075bc79
roman <lineblaze@mail.ru>
2024-10-08 22:22:04 +0300

    Proto v3

--- gen/go/thumbnail/thumbnail_grpc.pb.go
+++ gen/go/thumbnail/thumbnail_grpc.pb.go
@@ -18,86 +18,86 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
-// AuthClient is the client API for Auth service.
+// ThumbnailClient is the client API for Thumbnail service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type AuthClient interface {
+type ThumbnailClient interface {
 	GetThumbnail(ctx context.Context, in *ThumbnailRequest, opts ...grpc.CallOption) (*ThumbnailResponse, error)
 }
 
-type authClient struct {
+type thumbnailClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewAuthClient(cc grpc.ClientConnInterface) AuthClient {
-	return &authClient{cc}
+func NewThumbnailClient(cc grpc.ClientConnInterface) ThumbnailClient {
+	return &thumbnailClient{cc}
 }
 
-func (c *authClient) GetThumbnail(ctx context.Context, in *ThumbnailRequest, opts ...grpc.CallOption) (*ThumbnailResponse, error) {
+func (c *thumbnailClient) GetThumbnail(ctx context.Context, in *ThumbnailRequest, opts ...grpc.CallOption) (*ThumbnailResponse, error) {
 	out := new(ThumbnailResponse)
-	err := c.cc.Invoke(ctx, "/thumbnail.Auth/GetThumbnail", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/thumbnail.Thumbnail/GetThumbnail", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// AuthServer is the server API for Auth service.
-// All implementations must embed UnimplementedAuthServer
+// ThumbnailServer is the server API for Thumbnail service.
+// All implementations must embed UnimplementedThumbnailServer
 // for forward compatibility
-type AuthServer interface {
+type ThumbnailServer interface {
 	GetThumbnail(context.Context, *ThumbnailRequest) (*ThumbnailResponse, error)
-	mustEmbedUnimplementedAuthServer()
+	mustEmbedUnimplementedThumbnailServer()
 }
 
-// UnimplementedAuthServer must be embedded to have forward compatible implementations.
-type UnimplementedAuthServer struct {
+// UnimplementedThumbnailServer must be embedded to have forward compatible implementations.
+type UnimplementedThumbnailServer struct {
 }
 
-func (UnimplementedAuthServer) GetThumbnail(context.Context, *ThumbnailRequest) (*ThumbnailResponse, error) {
+func (UnimplementedThumbnailServer) GetThumbnail(context.Context, *ThumbnailRequest) (*ThumbnailResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetThumbnail not implemented")
 }
-func (UnimplementedAuthServer) mustEmbedUnimplementedAuthServer() {}
+func (UnimplementedThumbnailServer) mustEmbedUnimplementedThumbnailServer() {}
 
-// UnsafeAuthServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to AuthServer will
+// UnsafeThumbnailServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to ThumbnailServer will
 // result in compilation errors.
-type UnsafeAuthServer interface {
-	mustEmbedUnimplementedAuthServer()
+type UnsafeThumbnailServer interface {
+	mustEmbedUnimplementedThumbnailServer()
 }
 
-func RegisterAuthServer(s grpc.ServiceRegistrar, srv AuthServer) {
-	s.RegisterService(&Auth_ServiceDesc, srv)
+func RegisterThumbnailServer(s grpc.ServiceRegistrar, srv ThumbnailServer) {
+	s.RegisterService(&Thumbnail_ServiceDesc, srv)
 }
 
-func _Auth_GetThumbnail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Thumbnail_GetThumbnail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(ThumbnailRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(AuthServer).GetThumbnail(ctx, in)
+		return srv.(ThumbnailServer).GetThumbnail(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/thumbnail.Auth/GetThumbnail",
+		FullMethod: "/thumbnail.Thumbnail/GetThumbnail",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(AuthServer).GetThumbnail(ctx, req.(*ThumbnailRequest))
+		return srv.(ThumbnailServer).GetThumbnail(ctx, req.(*ThumbnailRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// Auth_ServiceDesc is the grpc.ServiceDesc for Auth service.
+// Thumbnail_ServiceDesc is the grpc.ServiceDesc for Thumbnail service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var Auth_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "thumbnail.Auth",
-	HandlerType: (*AuthServer)(nil),
+var Thumbnail_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "thumbnail.Thumbnail",
+	HandlerType: (*ThumbnailServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "GetThumbnail",
-			Handler:    _Auth_GetThumbnail_Handler,
+			Handler:    _Thumbnail_GetThumbnail_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
